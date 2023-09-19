8926b92188d68f56f0a3c03ea3db8b92428dad4b
pain <1998shubhamrex@gmail.com>
2023-09-19 17:51:07 +0530

    updated configuration to support shadcn

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,18 @@
 import Searchbar from "./searchbar";
 import { BsCartCheck } from "react-icons/bs";
 import { AiOutlineHome } from "react-icons/ai";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { BiFilterAlt } from "react-icons/bi";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-center  items-center  sticky top-0  bg-white py-2">
+    <div className="flex justify-evenly  items-center  sticky top-0  bg-white py-2">
+      <Sheet>
+        <SheetTrigger>
+          <BiFilterAlt />
+        </SheetTrigger>
+        <SheetContent side="left" className=" w-[250px] "></SheetContent>
+      </Sheet>
       <Searchbar />
       <div className=" gap-10 hidden  items-center justify-center  sm:flex relative sm:left-10 md:left-32">
         <AiOutlineHome size={24} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Bottombar from "../components/bottombar";
-import Navbar from "../components/navbar";
+import Navbar from "@/components/navbar";
 
 import ProductContainer from "../containers/product-container";
 
