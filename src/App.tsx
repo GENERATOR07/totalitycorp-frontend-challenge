f6a485e6e89368f29fd66f710801fae463a00b25
pain <1998shubhamrex@gmail.com>
2023-09-21 19:38:33 +0530

    fixed deployment bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/home";
-import Login from "./pages/login";
-import Cart from "./pages/cart";
-import Checkout from "./pages/checkout";
+import Home from "@/pages/home";
+import Login from "@/pages/login";
+import Cart from "@/pages/cart";
+import Checkout from "@/pages/checkout";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 
